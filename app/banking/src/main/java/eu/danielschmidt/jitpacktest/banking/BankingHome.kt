c78584ebd808eb110079cbabb4fd7917b6785c94
Daniel Schmidt <daniel.schmidt@senacor.com>
2023-03-14 15:44:11 +0100

    increase 0.7

--- app/banking/src/main/java/eu/danielschmidt/jitpacktest/banking/BankingHome.kt
+++ app/banking/src/main/java/eu/danielschmidt/jitpacktest/banking/BankingHome.kt
@@ -24,7 +24,7 @@ fun BankingHome() {
             .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "Banking Home v 0.6 + {${getSharedText()}}",
+            text = "Banking Home v 0.7 + {${getSharedText()}}",
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.align(Alignment.CenterHorizontally),
