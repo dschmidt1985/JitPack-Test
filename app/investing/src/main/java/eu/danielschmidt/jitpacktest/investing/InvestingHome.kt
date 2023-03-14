03a22030fdd838714329cbccb114afdd587d0a0e
Daniel Schmidt <daniel.schmidt@senacor.com>
2023-03-14 15:37:46 +0100

    increase 0.6

--- app/investing/src/main/java/eu/danielschmidt/jitpacktest/investing/InvestingHome.kt
+++ app/investing/src/main/java/eu/danielschmidt/jitpacktest/investing/InvestingHome.kt
@@ -24,7 +24,7 @@ fun InvestingHome() {
             .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "Investing Home v0.5 + ${getSharedText()}",
+            text = "Investing Home v0.6 + ${getSharedText()}",
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.align(Alignment.CenterHorizontally),
